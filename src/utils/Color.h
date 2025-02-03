f869179fde62f72001b147931637253c00c33bc2
Romain Plumaugat <plumaugat.romain78@gmail.com>
2025-02-03 23:06:51 +0100

    Add a color class

---
+++ src/utils/Color.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "pch.h"
+
+class Color {
+   public:
+    Color() : r(0.0f), g(0.0f), b(0.0f), a(255.0f) {};
+    Color(float r, float g, float b, float a = 255) : r(r), g(g), b(b), a(a) {};
+    ~Color() = default;
+
+    /* Attributes */
+    float r, g, b, a;
+
+    Color ToGLColor() const { return Color(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f); }
+
+    static const Color Black;
+    static const Color White;
+    static const Color Red;
+    static const Color Green;
+    static const Color Blue;
+};
+
+inline const Color Color::Black = Color(0.0f, 0.0f, 0.0f);
+inline const Color Color::White = Color(255.0f, 255.0f, 255.0f);
+inline const Color Color::Red = Color(255.0f, 0.0f, 0.0f);
+inline const Color Color::Green = Color(0.0f, 255.0f, 0.0f);
+inline const Color Color::Blue = Color(0.0f, 0.0f, 255.0f);
